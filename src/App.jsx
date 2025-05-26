38fb7e8e8ef915256acea27333d4d850a5ff580d
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-05-26 14:08:14 +0700

    up navbar, footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import ExistingRooms from "./components/room/ExistingRooms";
 import Home from "./components/home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EditRoom from "./components/room/EditRoom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Room from "./components/room/Room";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,13 +18,16 @@ function App() {
     <>
       <main>
         <Router>
+          <Navbar/>
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/edit-room/:roomId" element={<EditRoom />}/>
             <Route path="/existing-rooms" element={<ExistingRooms />}/>
             <Route path="/add-room" element={<AddRoom />}/>
+            <Route path="/room" element={<Room />}/>
           </Routes>
         </Router>
+        <Footer/>
       </main>
     </>
   );
